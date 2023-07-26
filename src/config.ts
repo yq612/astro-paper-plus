e11317ff32c168e460bb46bc4a9f0d2ad33be06f
You quan <youquan@YoudeMacBook-Pro.local>
2023-07-26 16:00:53 +0800

    feat: 修改默认的风格

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://jizhule.cn/",
+  author: "youquan",
+  desc: "web技术博客",
+  title: "記住了",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
